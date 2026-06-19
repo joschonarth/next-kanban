18683ad3a4edfe6c222c9fde314ab0629409361f
joschonarth <joschonarth@gmail.com>
2026-06-18 22:11:06 -0300

    feat: add Next caching to endpoints

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: true,
+
   logging: {
     fetches: {
       fullUrl: true,
--- src/http/list-issues-comments.ts
+++ src/http/list-issues-comments.ts
@@ -1,3 +1,4 @@
+import { cacheLife, cacheTag } from "next/cache"
 import { CommentsListResponseSchema } from "@/api/routes/list-issue-comments"
 import { clientEnv } from "@/env"
 
@@ -6,6 +7,11 @@ interface ListIssueCommentsParams {
 }
 
 export async function listIssueComments({ issueId }: ListIssueCommentsParams) {
+  "use cache"
+
+  cacheLife("minutes")
+  cacheTag(`issue-comments-${issueId}`)
+
   const url = new URL(
     `/api/issues/${issueId}/comments`,
     clientEnv.NEXT_PUBLIC_API_URL,
